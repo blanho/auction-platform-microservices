7dca272dc98e0098d202502bad813c57fe52fc09
blanho <h.baolan20025@gmail.com>
2026-04-02 00:50:44 +0700

    refactor: remove AuctionStatsGrpcService and trim AuctionGrpcService constructor

--- src/Services/Auction/Auction.Api/Grpc/AuctionGrpcService.cs
+++ src/Services/Auction/Auction.Api/Grpc/AuctionGrpcService.cs
@@ -6,14 +6,10 @@ namespace Auctions.Api.Grpc;
 public partial class AuctionGrpcService(
     IAuctionQueryRepository auctionQueryRepository,
     IAuctionWriteRepository auctionWriteRepository,
-    IAuctionUserRepository userRepository,
-    IAuctionAnalyticsRepository analyticsRepository,
     ILogger<AuctionGrpcService> logger)
     : AuctionGrpc.AuctionGrpcBase
 {
     private readonly IAuctionQueryRepository _auctionQueryRepository = auctionQueryRepository;
     private readonly IAuctionWriteRepository _auctionWriteRepository = auctionWriteRepository;
-    private readonly IAuctionUserRepository _userRepository = userRepository;
-    private readonly IAuctionAnalyticsRepository _analyticsRepository = analyticsRepository;
     private readonly ILogger<AuctionGrpcService> _logger = logger;
 }
